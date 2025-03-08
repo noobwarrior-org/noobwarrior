9c78ae4cea0460336c1440458e43053c197e88a6
Hattozo <29583664+Hattozo@users.noreply.github.com>
2025-03-08 01:17:09 -0500

    Made shell run in CLI version

--- NoGui/main.cpp
+++ NoGui/main.cpp
@@ -1,5 +1,18 @@
-#include <NoobWarrior/Tools/SavePlaceAssets.h>
+#define NOOBWARRIOR_NOSHELL_HINT 1
+#include <NoobWarrior/Shell.h>
 
-int main() {
-    return 0;
+#include <cstring>
+
+int main(int argc, char* argv[]) {
+    int ret = 0;
+    bool startShell = true;
+    for (int i = 0; i < argc; i++) {
+        if (strncmp(argv[i], "--no-shell", strlen(argv[i])) == 0)
+            startShell = false;
+    }
+    if (startShell) {
+        puts("\x1b[34mDon't want the CLI to open a shell? Start the program with the --no-shell flag.\x1b[0m");
+        ret = NoobWarrior::Shell::Open(stdout);
+    }
+    return ret;
 }
