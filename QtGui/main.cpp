0acca9553938116997993b7df3b1023326baccd3
Hattozo <29583664+Hattozo@users.noreply.github.com>
2025-03-11 20:40:10 -0400

    re-enabled native dialogs for macs it will display a more suited message when launched on a mac

--- QtGui/main.cpp
+++ QtGui/main.cpp
@@ -19,9 +19,6 @@
 int main(int argc, char *argv[]) {
     int ret = 1;
     QApplication app(argc, argv);
-#if defined(__APPLE__) // Native dialogs on Macs look nothing close to how they do on Windows, so we're turning that off.
-    app.setAttribute(Qt::ApplicationAttribute::AA_DontUseNativeDialogs, true);
-#endif
     CURLcode curlRet = curl_global_init(CURL_GLOBAL_ALL);
     if (curlRet != CURLE_OK) {
         QMessageBox::critical(nullptr, "Error", "Could not initialize curl");
@@ -38,10 +35,19 @@ int main(int argc, char *argv[]) {
         app.setStyleSheet(in.readAll());
     }
 #endif
+#if !defined(Q_OS_MACOS)
     QMessageBox::StandardButton res = QMessageBox::question(nullptr, "Warning",
         "What you are running is incomplete software. Nothing here is suitable for production. Things are bound to change, especially the way critical data is parsed by the program.\n\nBy clicking Yes, you agree to the statement that anything you try to create with this version of the software will eventually be corrupted due to unforeseen consequences.",
         QMessageBox::Yes | QMessageBox::No,
         QMessageBox::No);
+#else
+    QMessageBox msg;
+    msg.setText("You are running software that is likely broken");
+    msg.setInformativeText("Nothing here is suitable for production. Things are bound to change, especially the way critical data is parsed by the program.\n\nBy clicking Yes, you agree to the statement that anything you try to create with this version of the software will eventually be corrupted due to unforeseen consequences.");
+    msg.setStandardButtons(QMessageBox::Yes | QMessageBox::No);
+    msg.setDefaultButton(QMessageBox::No);
+    int res = msg.exec();
+#endif
     if (res != QMessageBox::Yes)
         goto cleanup;
     launcher.show();
