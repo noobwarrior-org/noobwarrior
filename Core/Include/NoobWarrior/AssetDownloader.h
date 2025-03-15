13c9ec41d0eedabe37b5bdbb2e6ec94a2863012d
Hattozo <29583664+Hattozo@users.noreply.github.com>
2025-03-15 18:53:25 -0400

    Added DA_DISABLE_TEMP_DIR

--- Core/Include/NoobWarrior/AssetDownloader.h
+++ Core/Include/NoobWarrior/AssetDownloader.h
@@ -16,10 +16,11 @@ namespace NoobWarrior {
     };
 
     enum AssetFlags {
-        DA_PRESERVE_AUTHORS = 1 << 0,
-        DA_PRESERVE_DATECREATED = 1 << 1, // Sets the Date Created metadata to be the asset's time of creation.
-        DA_PRESERVE_DATEMODIFIED = 1 << 2, // Sets the Date Modified metadata to be the time the asset was last updated.
-        DA_FIND_ASSET_IDS_IN_MODEL = 1 << 3 // If the Asset is a Model or a Place, it parses the file and checks for any asset URLs/IDs located within scripts and any properties.
+        DA_PRESERVE_AUTHORS = 1 << 0, // Sets the Authors metadata to be the name of the Asset's creator
+        DA_PRESERVE_DATECREATED = 1 << 1, // Sets the Date Created metadata to be the Asset's time of creation.
+        DA_PRESERVE_DATEMODIFIED = 1 << 2, // Sets the Date Modified metadata to be the time the Asset was last updated.
+        DA_FIND_ASSET_IDS_IN_MODEL = 1 << 3, // If the Asset is a Model or a Place, it parses the file and checks for any asset URLs/IDs located within scripts and any properties.
+        DA_DISABLE_TEMP_DIR = 1 << 4 // Prevents noobWarrior from temporarily storing the downloaded assets in a temp directory, instead it will just always store them in the output directory from the get-go.
     };
 
     struct DownloadAssetArgs {
