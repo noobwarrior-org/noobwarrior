d2af2894c8a7cdce3846d2c48c0ea2b5c190e2b9
Hattozo <29583664+Hattozo@users.noreply.github.com>
2025-03-15 18:40:04 -0400

    updated licensing info

--- Core/Include/NoobWarrior/NoobWarrior.h
+++ Core/Include/NoobWarrior/NoobWarrior.h
@@ -12,9 +12,12 @@
 #define NOOBWARRIOR_CONTRIBUTORS \
 "Hattozo\n"
 #define NOOBWARRIOR_ATTRIBUTIONS_BRIEF \
-"noobWarrior (https://github.com/noobWarrior-org/noobWarrior), licensed under the MIT license (https://opensource.org/license/mit/)\n" \
-"Roblox-File-Format (https://github.com/MaximumADHD/Roblox-File-Format), licensed under the MIT license (https://opensource.org/license/mit/)\n" \
-"yaml-cpp (https://github.com/jbeder/yaml-cpp), licensed under the MIT license (https://opensource.org/license/mit/)\n"
+"noobWarrior (https://github.com/noobWarrior-org/noobWarrior), licensed under the MIT License (https://opensource.org/license/mit/)\n" \
+"curl (https://github.com/curl/curl), licensed under the curl license (https://curl.se/docs/copyright.html)\n" \
+"SQLite (https://github.com/sqlite/sqlite/tree/master) - No license, this is under the public domain.\n" \
+"yaml-cpp (https://github.com/jbeder/yaml-cpp), licensed under the MIT License (https://opensource.org/license/mit/)\n" \
+"Roblox-File-Format (https://github.com/MaximumADHD/Roblox-File-Format), licensed under the MIT License (https://opensource.org/license/mit/)\n" \
+"famfamfam (Mark James) silk icons (https://github.com/markjames/famfamfam-silk-icons), licensed under the Creative Commons Attribution 2.5 License (http://creativecommons.org/licenses/by/2.5/)\n"
 
 #define NOOBWARRIOR_FREE_PTR(ptr) delete ptr; ptr = nullptr;
 #define NOOBWARRIOR_ARRAY_SIZE(arr) sizeof(arr) / sizeof(arr[0])
