6124e4930f314557f0a20672b2ffe04e4c71cd71
Hattozo <29583664+Hattozo@users.noreply.github.com>
2025-03-16 22:46:54 -0400

    Rename Net to NetClient

--- Core/Include/NoobWarrior/NetClient.h
+++ Core/Include/NoobWarrior/NetClient.h
@@ -1,5 +1,5 @@
 // === noobWarrior ===
-// File: Net.h
+// File: NetClient.h
 // Started by: Hattozo
 // Started on: 3/15/2025
 // Description: A thin layer over libcurl that makes it less annoying to use
--- Core/Source/NetClient.cpp
+++ Core/Source/NetClient.cpp
@@ -1,5 +1,5 @@
 // === noobWarrior ===
-// File: Net.cpp
+// File: NetClient.cpp
 // Started by: Hattozo
 // Started on: 3/15/2025
 // Description: A thin layer over libcurl that makes it less annoying to use
