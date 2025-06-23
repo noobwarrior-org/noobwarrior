755719c752fa047125c8863f8b401c8bff4ddd5f
Hattozo <29583664+Hattozo@users.noreply.github.com>
2025-06-23 15:50:22 -0400

    u can include noobwarrior.h without going into subdir

---
+++ Core/Include/NoobWarrior.h
@@ -0,0 +1,7 @@
+// === noobWarrior ===
+// File: NoobWarrior.h
+// Started by: Hattozo
+// Started on: 6/23/2025
+// Description: Allows you to include NoobWarrior.h without having to go into the subdirectory
+#pragma once
+#include "NoobWarrior/NoobWarrior.h"
