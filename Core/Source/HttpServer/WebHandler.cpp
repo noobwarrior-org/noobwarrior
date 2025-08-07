49cf94c064e06e5beeefaf3029c20f63c4637975
Hattozo <29583664+Hattozo@users.noreply.github.com>
2025-08-06 22:53:59 -0400

    working on frontend web

--- Core/Source/HttpServer/WebHandler.cpp
+++ Core/Source/HttpServer/WebHandler.cpp
@@ -19,6 +19,11 @@ using namespace NoobWarrior::HttpServer;
 WebHandler::WebHandler(Config *config, const std::filesystem::path &dir) : mConfig(config), Directory(dir) {}
 
 int WebHandler::OnRequest(mg_connection *conn, void *userdata) {
+    const mg_request_info *request_info = mg_get_request_info(conn);
+    const char* cookie_header = mg_get_header(conn, "Cookie");
+    char session_token[1024];
+    if (cookie_header) mg_get_cookie(cookie_header, ".SESSION", session_token, sizeof(session_token));
+
     char *fileName = static_cast<char*>(userdata);
 
 #ifdef DEBUG
@@ -44,6 +49,9 @@ int WebHandler::OnRequest(mg_connection *conn, void *userdata) {
     std::optional favicon = mConfig->GetKeyValue<std::string>("httpserver.branding.favicon");
 
     std::optional game_view_mode = mConfig->GetKeyValue<std::string>("httpserver.game_view_mode");
+    std::optional permissions_access_home = mConfig->GetKeyValue<std::string>("httpserver.permissions.access.home");
+    std::optional permissions_access_library = mConfig->GetKeyValue<std::string>("httpserver.permissions.access.library");
+    std::optional permissions_access_control_panel = mConfig->GetKeyValue<std::string>("httpserver.permissions.access.control_panel");
 
     // generate the template
     nlohmann::json data;
@@ -51,7 +59,19 @@ int WebHandler::OnRequest(mg_connection *conn, void *userdata) {
     data["branding"]["title"] = title.has_value() ? title.value() : "noobWarrior";
     data["branding"]["logo"] = logo.has_value() ? logo.value() : "/img/icon1024.png";
     data["branding"]["favicon"] = favicon.has_value() ? favicon.value() : "/img/favicon.ico";
+    data["permissions"]["access"]["home"] = permissions_access_home.has_value() ? permissions_access_home.value() : "user";
+    data["permissions"]["access"]["library"] = permissions_access_library.has_value() ? permissions_access_library.value() : "user";
+    data["permissions"]["access"]["control_panel"] = permissions_access_control_panel.has_value() ? permissions_access_control_panel.value() : "admin";
     data["game_view_mode"] = game_view_mode.has_value() ? game_view_mode.value() : "server";
+
+    if (session_token) {
+
+    }
+
+    data["user"]["id"] = -1;
+    data["user"]["name"] = "Guest";
+    data["user"]["rank"] = "guest";
+    data["user"]["headshot_thumbnail"] = "";
 
     data["servers"] = {
         {
@@ -66,6 +86,7 @@ int WebHandler::OnRequest(mg_connection *conn, void *userdata) {
     try {
         generatedBodyTemplate = inja::render(bodyFileBuf.str(), data);
     } catch (const inja::InjaError &e) {
+        OutEx(&std::cerr, "HttpServer::WebHandler", "Failed to render body template \"{}\" for IP address {}\nException Message: {}", fileName, request_info->remote_addr, e.what());
         mg_send_http_error(conn, 500, "Failed to render body template \"%s\"\nException Message: %s", fileName, e.what());
         return 500;
     }
@@ -80,14 +101,13 @@ int WebHandler::OnRequest(mg_connection *conn, void *userdata) {
     try {
         generatedMainTemplate = inja::render(mainFileBuf.str(), data);
     } catch (const inja::InjaError &e) {
+        OutEx(&std::cerr, "HttpServer::WebHandler", "Failed to render body template \"{}\" for IP address {}\nException Message: {}", fileName, request_info->remote_addr, e.what());
         mg_send_http_error(conn, 500, "Failed to render main template\nException Message: %s", e.what());
         return 500;
     }
 
     mg_send_http_ok(conn, "text/html", generatedMainTemplate.length());
 	mg_write(conn, generatedMainTemplate.c_str(), generatedMainTemplate.length());
-
-    Out("HttpServer::WebHandler", "Wrote to client {}", generatedMainTemplate);
 
     // cleanup
     mainFileInput.close();
