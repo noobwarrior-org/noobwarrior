245bebe42d75890ad3655f16b4f642c672e783de
hattozo <29583664+Hattozo@users.noreply.github.com>
2026-01-12 23:21:30 -0500

    identifier::getprotocol()

--- Core/Source/Identifier.cpp
+++ Core/Source/Identifier.cpp
@@ -5,6 +5,8 @@
 // Description:
 #include <NoobWarrior/Identifier.h>
 
+#define CHECK_PROTOCOL(str, type) if (protocolStr.compare(str) == 0) return type; // how to mask bad code 101
+
 using namespace NoobWarrior;
 
 Identifier::Identifier(const std::string &str, const std::string &cwd) : mStr(str) {
@@ -12,11 +14,20 @@ Identifier::Identifier(const std::string &str, const std::string &cwd) : mStr(st
 }
 
 ProtocolType Identifier::GetProtocol() {
-    
+    std::string protocolStr = GetProtocolString();
+    CHECK_PROTOCOL("http", ProtocolType::Http)
+    CHECK_PROTOCOL("https", ProtocolType::Https)
+    CHECK_PROTOCOL("plugin", ProtocolType::Plugin)
+    CHECK_PROTOCOL("data", ProtocolType::Data)
+    CHECK_PROTOCOL("rbxasset", ProtocolType::RbxAsset)
+    CHECK_PROTOCOL("rbxthumb", ProtocolType::RbxThumb)
+    return ProtocolType::Unsupported;
 }
 
 std::string Identifier::GetProtocolString() {
     int pos = mStr.find("://");
-    if (pos)
+    if (pos) {
         std::string protocol = mStr.substr(0 , pos);
+        return protocol;
+    } else return mStr;
 }
