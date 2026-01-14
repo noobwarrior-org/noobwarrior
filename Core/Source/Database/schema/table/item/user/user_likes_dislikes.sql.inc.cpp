c50620548f627327645f2c92102da3d11da5e85e
hattozo <29583664+Hattozo@users.noreply.github.com>
2026-01-14 00:27:03 -0500

    Changed license to LGPLv3

--- Core/Source/Database/schema/table/item/user/user_likes_dislikes.sql.inc.cpp
+++ Core/Source/Database/schema/table/item/user/user_likes_dislikes.sql.inc.cpp
@@ -1,3 +1,22 @@
+/*
+ * Copyright (C) 2026 Hattozo
+ *
+ * This file is part of noobWarrior.
+ *
+ * noobWarrior is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * noobWarrior is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with noobWarrior; if not, see
+ * <https://www.gnu.org/licenses/>.
+ */
 /**
  * Fill in only one of these "AssetId", "BadgeId", "BundleId", "GroupId", "PassId", "UniverseId" for each record
  * if you don't want the universe to explode. Doing polymorphic association in SQL is very stupid
--- QtGui/DatabaseEditor/FileManagerWidget.h
+++ QtGui/DatabaseEditor/FileManagerWidget.h
@@ -1,3 +1,22 @@
+/*
+ * Copyright (C) 2026 Hattozo
+ *
+ * This file is part of noobWarrior.
+ *
+ * noobWarrior is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * noobWarrior is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with noobWarrior; if not, see
+ * <https://www.gnu.org/licenses/>.
+ */
 // === noobWarrior ===
 // File: FileManagerWidget.h
 // Started by: Hattozo
