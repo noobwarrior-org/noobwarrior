589b3e9628f473b95660cb82d44c1cfd356f3f21
hattozo <29583664+Hattozo@users.noreply.github.com>
2026-03-29 21:25:46 -0400

    a

--- Core/Include/NoobWarrior/BaseConfig.h
+++ Core/Include/NoobWarrior/BaseConfig.h
@@ -26,6 +26,8 @@
 #include <NoobWarrior/Log.h>
 
 #include <NoobWarrior/Lua/LuaState.h>
+
+#include <sol/sol.hpp>
 #include <nlohmann/json.hpp>
 
 #include <filesystem>
@@ -80,7 +82,7 @@ public:
     void SetKeyComment(const char *key, const char *comment);
 
     template <typename T>
-        void SetKeyValue(const std::string &key, T value) {
+    void SetKeyValue(const std::string &key, T value) {
         if (key.empty()) {
             Out("BaseConfig", "Error setting value for key \"{}\": key is empty", key);
             return;
@@ -91,7 +93,7 @@ public:
             const sol::error err = res;
             Out("BaseConfig", "Error setting value for key \"{}\": \"{}\"", key, err.what());
         }
-        (*mLua)["__REG_BUF"] = sol::nil;
+        (*mLua)["__REG_BUF"] = sol::lua_nil;
     }
 
     template <typename T>
@@ -100,13 +102,15 @@ public:
             Out("BaseConfig", "Error getting value for key \"{}\": key is empty", key);
             return std::nullopt;
         }
-        sol::protected_function_result res = mLua->safe_script(std::format("return {}.{}", mGlobalName, key));
+        sol::protected_function_result res = mLua->safe_script(std::format("return {}.{}", mGlobalName, key), sol::script_pass_on_error);
         if (!res.valid()) {
-            const sol::error err = res;
-            Out("BaseConfig", "Error getting value for key \"{}\": \"{}\"", key, err.what());
             return std::nullopt;
         }
-        return res.get<std::optional<T>>();
+        sol::object obj = res.get<sol::object>();
+        if (!obj.is<T>()) {
+            return std::nullopt;
+        }
+        return obj.as<T>();
     }
 
     template <typename T>
@@ -123,68 +127,5 @@ protected:
     LuaState*               mLua;
 
     std::mutex AccessConfigMutex;
-
-    class Namespace {
-    public:
-        Namespace();
-
-    };
-
-    Namespace &GetNamespace(const std::string &namespaceStr);
-
-    /*std::map<std::any, std::pair<const char*, const char*>> mSerializedProperties;
-
-    template <typename T>
-    void SerializeProperty(T *property, const char *key, const char *comment = nullptr) {
-        mSerializedProperties[property] = { key, comment };
-    }*/
-
-    /*
-    template <typename T>
-    void DeserializeProperty(T *property, const char *key) {
-        int stackSize = 0;
-        char *token = strtok(const_cast<char*>(key), ".");
-        while (token != nullptr) {
-            stackSize++;
-            lua_getfield(mLuaState, -1, token);
-            token = strtok(nullptr, " ");
-            // so we haven't yet reached the end of our tokenizer, but somehow this is not a table? that's not right
-            if (token != nullptr && !lua_istable(mLuaState, -1)) {
-                luaL_error(mLuaState, R"(expected type "table" for token "%s", got "%s")", token, lua_typename(mLuaState, lua_type(mLuaState, -1)));
-                lua_pop(mLuaState, stackSize);
-                return;
-            }
-        }
-        if (lua_isnil(mLuaState, -1))
-            goto done;
-        if constexpr (std::is_same_v<T, std::string>) {
-            if (lua_type(mLuaState, -1) == LUA_TSTRING)
-                *property = std::string(lua_tostring(mLuaState, -1));
-        } else if constexpr (std::is_same_v<T, int> || std::is_enum_v<T>) {
-            if (lua_type(mLuaState, -1) == LUA_TNUMBER)
-                *property = static_cast<T>(lua_tointeger(mLuaState, -1));
-        } else if constexpr (std::is_same_v<T, bool>) {
-            if (lua_type(mLuaState, -1) == LUA_TBOOLEAN)
-                *property = lua_toboolean(mLuaState, -1);
-        }
-    done:
-        lua_pop(mLuaState, stackSize);
-    }
-
-    template <typename T>
-    void SerializeProperty(T property, const char *key, const char *comment = nullptr) {
-        int level = 0;
-        char *token = strtok(const_cast<char*>(key), ".");
-        while (token != nullptr) {
-            level++;
-            std::string spaces(level * 4, ' ');
-            *mFileOutput << spaces << token << " = {\n" << spaces << "}";
-            token = strtok(nullptr, " ");
-            if (token != nullptr) {
-
-            }
-        }
-    }
-    */
 };
 }
