120215c6342e0b2ac2e4b6bbdcd247ad9a723b9e
hattozo <29583664+hattozo@users.noreply.github.com>
2026-05-31 17:25:19 -0400

    cool sparkle effect + music

--- Tenfoot/fade_out.gd
+++ Tenfoot/fade_out.gd
@@ -3,6 +3,7 @@ extends ColorRect
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	self.visible = true
+	await get_tree().create_timer(2.0).timeout
 	var tween = create_tween()
-	tween.tween_property(self, "color", Color(0, 0 ,0, 0), 3.0)
+	tween.tween_property(self, "color", Color(0, 0, 0, 0), 3.0)
 	tween.play()
--- Tenfoot/tenfoot.gd
+++ Tenfoot/tenfoot.gd
@@ -1,7 +1,26 @@
 extends Control
 
+func fade_children(node : Node) -> void:
+	for child in node.get_children():
+		if child is Control:
+			child.modulate.a = 0.0
+			var tween = create_tween()
+			tween.tween_property(child, "modulate:a", 1.0, 0.5)
+			
+			if child.get_child_count() > 0:
+				fade_children(node)
+
+func ShowMenu(path : String) -> void:
+	var scene = load(path)
+	var inst = scene.instantiate()
+	add_child(inst)
+	fade_children(inst)
+	pass
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	$Music.play()
+	ShowMenu("res://menu/main.tscn")
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
