39f86a32d5a4490b14723d7125152b83d10316f3
hattozo <29583664+hattozo@users.noreply.github.com>
2026-05-31 20:21:55 -0400

    godot

--- Tenfoot/tenfoot.gd
+++ Tenfoot/tenfoot.gd
@@ -1,29 +1,41 @@
 extends Control
 
-func fade_children(node : Node) -> void:
+var music : Array = ["res://sounds/music/music1.ogg", "res://sounds/music/music2.ogg"]
+var currentMenu : Node
+
+func fade_children(node : Node, initialVal : float, finalVal : float) -> void:
 	for child in node.get_children():
 		if child is Control:
-			child.modulate.a = 0.0
+			child.modulate.a = initialVal
 			var tween = create_tween()
-			tween.tween_property(child, "modulate:a", 1.0, 0.5)
+			tween.tween_property(child, "modulate:a", finalVal, 0.5)
 			
 			if child.get_child_count() > 0:
-				fade_children(node)
+				fade_in_children(child)
+
+func fade_in_children(node : Node) -> void:
+	fade_children(node, 0.0, 1.0)
+				
+func fade_out_children(node : Node) -> void:
+	fade_children(node, 1.0, 0.0)
 
 func ShowMenu(path : String) -> void:
+	if currentMenu != null:
+		fade_out_children(currentMenu)
+		await get_tree().create_timer(1.0).timeout
+		currentMenu.queue_free()
 	var scene = load(path)
-	var inst = scene.instantiate()
-	add_child(inst)
-	fade_children(inst)
-	pass
+	var menu : Node = scene.instantiate()
+	currentMenu = menu
+	add_child(menu)
+	fade_in_children(menu)
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	var stream = load(music.pick_random())
+	$Music.stream = stream
 	$Music.play()
-	ShowMenu("res://menu/main.tscn")
-	pass # Replace with function body.
+	ShowMenu("res://menu/main/main.tscn")
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 
