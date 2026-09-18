cd8eb85a75858afb1f5fdbcc0b4eb2cf6357e0a2
hattozo <29583664+hattozo@users.noreply.github.com>
2026-09-18 18:21:46 -0400

    redoing index.lua

--- AppDistribution/priv-plugins/docs/texts/index.lua
+++ AppDistribution/priv-plugins/docs/texts/index.lua
@@ -1,20 +1,32 @@
 return {
-    ["docs.getting_started"] = {
-        ["docs.getting_started.preface"] = "001-getting-started/001-preface.md",
-        ["docs.getting_started.introduction"] = "001-getting-started/002-introduction.md"
+    {
+        Title = "docs.getting_started",
+        Children = {
+            {
+                Title = "docs.getting_started.preface",
+                File = "001-getting-started/001-preface.md"
+            },
+            {
+                Title = "docs.getting_started.introduction",
+                File = "001-getting-started/002-introduction.md"
+            }
+        }
     },
-    ["docs.user_guides"] = {
-        
+    {
+        Title = "docs.user_guides"
     },
-    ["docs.api_reference"] = {
-        ["docs.api_reference.lua"] = {
-
-        },
-        ["docs.api_reference.c"] = {
-
-        },
-        ["docs.api_reference.cpp"] = {
-
+    {
+        Title = "docs.api_reference",
+        Children = {
+            {
+                Title = "docs.api_reference.lua"
+            },
+            {
+                Title = "docs.api_reference.cpp"
+            },
+            {
+                Title = "docs.api_reference.c"
+            }
         }
     }
 }
